4354dfedf02fc37d23badb0c7478532037eeef25
Juan Espitia <juanespitia@outlook.com>
2020-12-03 17:38:02 -0500

    Add Login in API

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 const user = require('../components/users/routes');
 const company = require('../components/company/routes');
+const loginRoutes = require('./auth')
 
 const routes = function(app) {
     app.use('/', user)
     app.use('/', company)
+    app.use('/', loginRoutes)
     
 }
 
