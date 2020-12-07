417d103f1a925480b7edf8d019b198d371c475b3
Juan Espitia <juanespitia@outlook.com>
2020-12-06 23:47:36 -0500

    add new route in tickets

--- src/components/tickets/routes.js
+++ src/components/tickets/routes.js
@@ -5,7 +5,7 @@ const tickets = require('./controller')
 
 router.get('/ticket', tickets.getTickets)
 router.get('/ticket/:id', tickets.getOneTicket)
-router.get('/ticket/:storie', tickets.getStorieTicket)
+router.get('/tickets/:storie', tickets.getStorieTicket)
 router.post('/ticket', tickets.postTicket)
 router.patch('/ticket/:id', tickets.updateTicket)
 router.delete('/ticket/:id',  tickets.deleteTicket)
